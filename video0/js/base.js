68bc9c05c816142d29056f6348f745c4e523ed25
Brynjar Harðarson <brynjar@gmail.com>
2019-09-05 23:45:32 +0000

    js fix

--- video0/js/base.js
+++ video0/js/base.js
@@ -2,7 +2,6 @@
 // External links should target _blank by default (if not already set to something).
 var host = window.location.host;
 var links = document.getElementsByTagName('a');
-console.log(links)
 for (var i = 0; i < links.length; i++) {
     if (links[i].host !== host && !links[i].getAttribute('target')) {
         links[i].setAttribute('target', '_blank');
