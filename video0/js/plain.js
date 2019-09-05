8ab2e349421469fe2f2df3687be4dcc8b3fe00aa
Brynjar Harðarson <brynjar@gmail.com>
2019-09-05 18:20:28 +0000

    miscx

--- video0/js/plain.js
+++ video0/js/plain.js
@@ -1,6 +1,33 @@
-$("a").click(function() {
+var hasBlured = false;
+var timecode = [];
+
+$("video").each(function() {
+	var src = $(this).find("source").attr("src");
+	$(this).attr("data-src", src);
+})
+
+function blur() {
+	hasBlured = true;
+	timecode = [];
 	$("video").each(function() {
-		//$(this).get(0).pause();
-		//$(this).attr("src", "");
+		$(this).get(0).pause();
+		var currentTime = $(this).get(0).currentTime;
+		timecode.push(currentTime);
+		$(this).attr("src", "");
 	})
-});
+}
+function focus() {
+	if (timecode.length && hasBlured) {
+		hasBlured = false;
+		var x = 0;
+		$("video").each(function() {
+			var src = $(this).attr("data-src");
+			$(this).attr("src", src);
+			$(this).get(0).currentTime = timecode[x];
+			x += 1;
+		})
+	}
+}
+
+$("a").click(blur);
+window.onfocus = focus;
