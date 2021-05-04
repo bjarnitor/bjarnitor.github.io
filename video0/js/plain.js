71042d925c16b9c2e281ae9476a80efc4b0ddda8
Monkey King <gautamadude@gmail.com>
2021-05-04 11:12:42 +0000

    frame remove didnt work

--- video0/js/plain.js
+++ video0/js/plain.js
@@ -32,16 +32,15 @@ function focus() {
 /*$(".special video").each(function() {
 	$(this).get(0).currentTime = 60*6;
 });*/
-console.log("SDFDSFS")
-document.getElementById('s1').addEventListener('loadedmetadata', function() {
+
+/*document.getElementById('s1').addEventListener('loadedmetadata', function() {
 	this.currentTime = 60*6;
-	console.log("PLAY")
 	this.play();
 }, false);
 document.getElementById('s2').addEventListener('loadedmetadata', function() {
 	this.currentTime = 60*6;
 	this.play();
-}, false);
+}, false);*/
 
 //$("a").click(blur);
 window.onblur = blur;
